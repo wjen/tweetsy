8b15a07b81f23e4d496578c0c741a1b47e922f15
Wen Jen <wjen1214@gmail.com>
2020-11-16 17:53:30 -0800

    add proxy and error jsx

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className="ui active centered large inline loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
